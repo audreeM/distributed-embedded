ad15bccf973f46e47e6b7c9ab274039efad3fb33
Crytsal Huang <ruizhenh@andrew.cmu.edu>
2026-09-26 16:31:25 -0400

    thread initialization and define thread priority

---
+++ lab2_stm32/include/config.h
@@ -0,0 +1,6 @@
+#define STACK_SZ 1024
+#define PRIO_RX 1
+#define PRIO_MONITOR 2
+#define PRIO_CTRL 3
+#define PRIO_HB
+#define PRIO_FORCE 5
--- lab2_stm32/src/main.c
+++ lab2_stm32/src/main.c
@@ -1,50 +1,24 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include "config.h"
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-
-///TODO frame parsing, status returns, and the 150 ms fail-safe timer
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+K_THREAD_DEFINE(rx_tid,      STACK_SZ, rx_thread,      NULL, NULL, NULL, PRIO_RX,      0, SYS_FOREVER_MS);
+K_THREAD_DEFINE(monitor_tid, STACK_SZ, monitor_thread, NULL, NULL, NULL, PRIO_MONITOR, 0, SYS_FOREVER_MS);
+K_THREAD_DEFINE(ctrl_tid,    STACK_SZ, ctrl_thread,    NULL, NULL, NULL, PRIO_CTRL,    0, SYS_FOREVER_MS);
+K_THREAD_DEFINE(hb_tid,      STACK_SZ, hb_thread,      NULL, NULL, NULL, PRIO_HB,      0, SYS_FOREVER_MS);
+K_THREAD_DEFINE(force_tid,   STACK_SZ, force_thread,   NULL, NULL, NULL, PRIO_FORCE,   0, SYS_FOREVER_MS);
 
 int main(void)
 {
-	int ret;
-	bool led_state = true;
-
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
+	for (int i = 0; i < TP_COUNT; i++) {
+		gpio_pin_configure_dt(&tp[i], GPIO_OUTPUT_INACTIVE);
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
+	k_thread_start(rx_tid);
+	k_thread_start(monitor_tid);
+	k_thread_start(ctrl_tid);
+	k_thread_start(hb_tid);
+	k_thread_start(force_tid);
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
-
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
-	}
 	return 0;
 }
